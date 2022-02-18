4ee4648ca2da1738e46fbb3ae8c65c03b0b69908
Vladimir Kishlaly <vladimir@kishlaly.com>
2022-02-18 09:03:10 +0100

    update

--- src/main/java/com/kishlaly/ta/Main.java
+++ src/main/java/com/kishlaly/ta/Main.java
@@ -1,19 +1,26 @@
 package com.kishlaly.ta;
 
 import com.kishlaly.ta.analyze.TaskType;
+import com.kishlaly.ta.analyze.testing.TaskTester;
 import com.kishlaly.ta.analyze.testing.sl.*;
-import com.kishlaly.ta.analyze.testing.tp.TakeProfitDisabled;
-import com.kishlaly.ta.analyze.testing.tp.TakeProfitKeltner;
+import com.kishlaly.ta.analyze.testing.tp.TakeProfitFixedKeltnerTop;
 import com.kishlaly.ta.analyze.testing.tp.TakeProfitStrategy;
+import com.kishlaly.ta.analyze.testing.tp.TakeProfitVolatileKeltnerTop;
+import com.kishlaly.ta.model.HistoricalTesting;
 import com.kishlaly.ta.model.Timeframe;
 import com.kishlaly.ta.utils.Context;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
-import static com.kishlaly.ta.analyze.TaskRunner.run;
-import static com.kishlaly.ta.analyze.TaskType.*;
+import static com.kishlaly.ta.analyze.TaskType.THREE_DISPLAYS_BUY_TYPE2;
 import static com.kishlaly.ta.analyze.testing.TaskTester.test;
-import static com.kishlaly.ta.cache.CacheBuilder.buildCache;
 
 /**
  * @author Vladimir Kishlaly
@@ -32,12 +39,12 @@ public class Main {
         };
 
 //        Context.source = "symbols/sp500.txt";
-//        Context.source = "symbols/screener_2.txt";
+        Context.source = "symbols/screener_2.txt";
 //        Context.source = "symbols/screener_many.txt";
-        Context.source = "symbols/naga.txt";
-//        Context.testOnly = new ArrayList<String>() {{
-//            add("SEE");
-//        }};
+//        Context.source = "symbols/naga.txt";
+        Context.testOnly = new ArrayList<String>() {{
+            add("DDOG");
+        }};
 
 
         TaskType[] tasks = {
@@ -49,25 +56,34 @@ public class Main {
 
 //        buildCache(timeframes, tasks, false);
 //        checkCache(timeframes, tasks);
-        run(timeframes, tasks);
-//        testFixed(timeframes, tasks, new StopLossFixedPrice(0.27), new TakeProfitKeltner(100));
-//        testVolatile(timeframes, tasks);
+//        run(timeframes, tasks);
+        testSimple(timeframes, tasks);
 
     }
 
-    private static void testFixed(Timeframe[][] timeframes, TaskType[] tasks, StopLossStrategy stopLossStrategy, TakeProfitStrategy takeProfitStrategy) {
-        Context.stopLossStrategy = stopLossStrategy;
-        Context.takeProfitStrategy = takeProfitStrategy;
-        test(timeframes, tasks);
+    private static void testSimple(Timeframe[][] timeframes, TaskType[] tasks) {
+        List<HistoricalTesting> result = new ArrayList<>();
+        getSLStrategies().forEach(stopLossStrategy -> {
+            getTPStrategies().forEach(takeProfitStrategy -> {
+                Context.stopLossStrategy = stopLossStrategy;
+                Context.takeProfitStrategy = takeProfitStrategy;
+                System.out.println(stopLossStrategy + " / " + takeProfitStrategy);
+                result.addAll(test(timeframes, tasks));
+            });
+        });
+        Collections.sort(result, Comparator.comparing(HistoricalTesting::getBalance));
+
     }
 
-    private static void testVolatile(Timeframe[][] timeframes, TaskType[] tasks, StopLossStrategy stopLossStrategy, TakeProfitStrategy takeProfitStrategy) {
-        Context.stopLossStrategy = stopLossStrategy;
-        Context.takeProfitStrategy = takeProfitStrategy;
-        test(timeframes, tasks);
+    private static void writeToFile(String name, String content) {
+        try {
+            Files.write(Paths.get("tests/" + name + ".txt"), content.toString().getBytes());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    private static void testFixedMany(Timeframe[][] timeframes, TaskType[] tasks) {
+    private static void testMass(Timeframe[][] timeframes, TaskType[] tasks) {
         Context.massTesting = true;
 
         StopLossStrategy stopLossStrategy = new StopLossFixedPrice(0.27);
@@ -75,10 +91,33 @@ public class Main {
 
         Context.takeProfitStrategies = new ArrayList<>();
         for (int i = 80; i <= 100; i++) {
-            TakeProfitStrategy tp = new TakeProfitKeltner(i);
+            TakeProfitStrategy tp = new TakeProfitFixedKeltnerTop(i);
             Context.takeProfitStrategies.add(tp);
         }
         test(timeframes, tasks);
+    }
+
+    private static List<StopLossStrategy> getSLStrategies() {
+        return new ArrayList<StopLossStrategy>() {{
+            add(new StopLossFixedPrice(0.27));
+            add(new StopLossFixedKeltnerBottom());
+            add(new StopLossVolatileKeltnerBottom());
+            add(new StopLossVolatileLocalMin(0.27));
+            add(new StopLossVolatileATR());
+        }};
+    }
+
+    private static List<TakeProfitStrategy> getTPStrategies() {
+        return new ArrayList<TakeProfitStrategy>() {{
+            add(new TakeProfitFixedKeltnerTop(70));
+            add(new TakeProfitFixedKeltnerTop(80));
+            add(new TakeProfitFixedKeltnerTop(90));
+            add(new TakeProfitFixedKeltnerTop(100));
+            add(new TakeProfitVolatileKeltnerTop(70));
+            add(new TakeProfitVolatileKeltnerTop(80));
+            add(new TakeProfitVolatileKeltnerTop(90));
+            add(new TakeProfitVolatileKeltnerTop(100));
+        }};
     }
 
 }
