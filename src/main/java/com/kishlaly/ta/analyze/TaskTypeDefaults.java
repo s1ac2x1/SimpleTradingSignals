641b6c40243e2f74e413332ce1e5161161a68cb3
Vladimir Kishlaly <vladimir@kishlaly.com>
2022-03-12 10:47:09 +0100

    update

--- src/main/java/com/kishlaly/ta/analyze/TaskTypeDefaults.java
+++ src/main/java/com/kishlaly/ta/analyze/TaskTypeDefaults.java
@@ -54,7 +54,27 @@ public class TaskTypeDefaults {
             add(new Long_ScreenTwo_EMA_TwoBarsAscendingAndCrossing());
             add(new Long_ScreenTwo_FilterLateEntry());
         }};
+    }
 
+    // проверить buy стратегию (вдохновитель [D] CFLT 20 Dec 2021)
+    // первый экран - подумать TODO
+    // второй экран -
+    //    перепроданность ниже 20 у трех значений медленной линии стохастика и она повышается
+    //    последние три столбика гистограммы повышаются
+    //    два из трех последних баров зеленые
+    //    последние два бара повышаются (quote.low & quote.high)
+    //    последние два бара полностью ниже ЕМА13
+    // вход на 7 центов выше закрытия последнего бара
+    // TP на середине верхней половины канала Кельтнера
+    public List<TaskBlock> threeDisplaysType3() {
+        return new ArrayList<TaskBlock>() {{
+            add(new ScreenValidation());
+            add(new ScreenOneStrictTrendCheck());
+
+            add(new Long_ScreenTwo_Stoch_ThreeValuesAscendingFromStrongOversold());
+            add(new Long_ScreenTwo_MACD_ThreeBarsBelowZeroAndAscending());
+            add(new Long_ScreenTwo_TwoGreenQuotes());
+        }};
     }
 
 }
