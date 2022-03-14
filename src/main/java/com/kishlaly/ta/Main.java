befd4ecc8eec1ff5b1a5688de86d9185ae459d7a
Vladimir Kishlaly <vladimir@kishlaly.com>
2022-03-14 16:31:35 +0100

    update

--- src/main/java/com/kishlaly/ta/Main.java
+++ src/main/java/com/kishlaly/ta/Main.java
@@ -48,14 +48,14 @@ public class Main {
                 THREE_DISPLAYS_BUY, // лучше работает для DAY-HOUR
                 //FIRST_TRUST_MODEL, // искать на S&P500
         };
-        THREE_DISPLAYS_BUY.setBlocks(TaskTypeDefaults.threeDisplaysBuyType2());
+        THREE_DISPLAYS_BUY.setBlocks(TaskTypeDefaults.threeDisplaysBuyType4());
 
 //        buildCache(timeframes, tasks, false);
 //        findBestStrategyForSymbols();
 //        checkCache(timeframes, tasks);
-        run(timeframes, tasks, false);
+//        run(timeframes, tasks, false);
 //        runBest(timeframes);
-//        testOneStrategy(timeframes, tasks, new StopLossFixedPrice(0.27), new TakeProfitFixedKeltnerTop(100));
+        testOneStrategy(timeframes, tasks, new StopLossFixedPrice(0.27), new TakeProfitFixedKeltnerTop(100));
 //        buildTasksAndStrategiesSummary(timeframes, tasks, new StopLossFixedPrice(0.27), new TakeProfitFixedKeltnerTop(100));
 //        buildTasksAndStrategiesSummary(timeframes, tasks, null, null);
     }
