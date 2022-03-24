79a79255ad10ad97e654abcfa4b901f2015122fd
Vladimir Kishlaly <vladimir@kishlaly.com>
2022-03-24 11:49:30 +0100

    update

--- src/main/java/com/kishlaly/ta/Main.java
+++ src/main/java/com/kishlaly/ta/Main.java
@@ -33,14 +33,14 @@ public class Main {
         };
 
         Context.source = new SymbolsSource[]{
-                SymbolsSource.SP500,
+                //SymbolsSource.SP500,
                 //SymbolsSource.NAGA,
-                //SymbolsSource.SCREENER_FILTERED
+                SymbolsSource.SCREENER_FILTERED
                 //SymbolsSource.TEST
         };
-        Context.testOnly = new ArrayList<String>() {{
-            add("AMD");
-        }};
+//        Context.testOnly = new ArrayList<String>() {{
+//            add("AMD");
+//        }};
         Context.symbols = getSymbols();
         Context.yearsToAnalyze = 5;
 
@@ -51,24 +51,24 @@ public class Main {
 //        run(timeframes, THREE_DISPLAYS_BUY, false, new ThreeDisplays_Buy_1());
 //        run(timeframes, THREE_DISPLAYS_BUY, false, new ThreeDisplays_Buy_2());
 //        run(timeframes, THREE_DISPLAYS_BUY, false, new ThreeDisplays_Buy_4());
-//        run(timeframes, THREE_DISPLAYS_BUY, false, new ThreeDisplays_Buy_8());
+        run(timeframes, THREE_DISPLAYS_BUY, false, new ThreeDisplays_Buy_8());
 //        runBest(timeframes);
 //        testOneStrategy(timeframes, THREE_DISPLAYS_BUY, new ThreeDisplays_Buy_4(), new StopLossFixedPrice(0.27), new TakeProfitFixedKeltnerTop(70));
 
-        buildTasksAndStrategiesSummary(
-                timeframes,
-                THREE_DISPLAYS_BUY,
-                new ArrayList<BlocksGroup>(){{
-                    add(new ThreeDisplays_Buy_1());
-                    add(new ThreeDisplays_Buy_2());
-                    add(new ThreeDisplays_Buy_3());
-                    add(new ThreeDisplays_Buy_4());
-                    add(new ThreeDisplays_Buy_5());
-                    add(new ThreeDisplays_Buy_6());
-                    add(new ThreeDisplays_Buy_7());
-                    add(new ThreeDisplays_Buy_8());
-                }},
-                new StopLossFixedPrice(0.27), new TakeProfitFixedKeltnerTop(70));
+//        buildTasksAndStrategiesSummary(
+//                timeframes,
+//                THREE_DISPLAYS_BUY,
+//                new ArrayList<BlocksGroup>(){{
+//                    add(new ThreeDisplays_Buy_1());
+//                    add(new ThreeDisplays_Buy_2());
+//                    add(new ThreeDisplays_Buy_3());
+//                    add(new ThreeDisplays_Buy_4());
+//                    add(new ThreeDisplays_Buy_5());
+//                    add(new ThreeDisplays_Buy_6());
+//                    add(new ThreeDisplays_Buy_7());
+//                    add(new ThreeDisplays_Buy_8());
+//                }},
+//                new StopLossFixedPrice(0.27), new TakeProfitFixedKeltnerTop(70));
 
     }
 
