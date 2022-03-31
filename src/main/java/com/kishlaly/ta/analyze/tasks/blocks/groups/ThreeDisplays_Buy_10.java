736c50618588d28a9122830b9383c7b580ff1577
Vladimir Kishlaly <vladimir@kishlaly.com>
2022-03-31 16:06:21 +0200

    update

--- src/main/java/com/kishlaly/ta/analyze/tasks/blocks/groups/ThreeDisplays_Buy_10.java
+++ src/main/java/com/kishlaly/ta/analyze/tasks/blocks/groups/ThreeDisplays_Buy_10.java
@@ -3,6 +3,10 @@ package com.kishlaly.ta.analyze.tasks.blocks.groups;
 import com.kishlaly.ta.analyze.tasks.ThreeDisplays;
 import com.kishlaly.ta.analyze.tasks.blocks.TaskBlock;
 import com.kishlaly.ta.analyze.tasks.blocks.commons.ScreenBasicValidation;
+import com.kishlaly.ta.analyze.tasks.blocks.two.Long_ScreenTwo_Bollinger_Low_X_Of_Y_LastBarsCrossed;
+import com.kishlaly.ta.analyze.tasks.blocks.two.Long_ScreenTwo_MACD_TwoBelowZeroAndAscending;
+import com.kishlaly.ta.analyze.tasks.blocks.two.Long_ScreenTwo_Stoch_D_LastAscending;
+import com.kishlaly.ta.analyze.tasks.blocks.two.Long_ScreenTwo_Stoch_D_TwoBelow_X;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,10 +25,15 @@ public class ThreeDisplays_Buy_10 implements BlocksGroup {
     public List<TaskBlock> blocks() {
         ThreeDisplays.Config.BOLLINGER_TOTAL_BARS_CHECK = 3;
         ThreeDisplays.Config.BOLLINGER_CROSSED_BOTTOM_BARS = 1;
+        ThreeDisplays.Config.STOCH_CUSTOM = 30;
 
         return new ArrayList<TaskBlock>() {{
             add(new ScreenBasicValidation());
 
+            add(new Long_ScreenTwo_Bollinger_Low_X_Of_Y_LastBarsCrossed());
+            add(new Long_ScreenTwo_MACD_TwoBelowZeroAndAscending());
+            add(new Long_ScreenTwo_Stoch_D_TwoBelow_X());
+            add(new Long_ScreenTwo_Stoch_D_LastAscending());
         }};
 
     }
