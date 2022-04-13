4f610bd3e11fb238613d96122ddcaac9e5477270
Vladimir Kishlaly <vladimir@kishlaly.com>
2022-04-13 11:03:19 +0200

    update

--- src/main/java/com/kishlaly/ta/utils/SpreadsheetUtils.java
+++ src/main/java/com/kishlaly/ta/utils/SpreadsheetUtils.java
@@ -14,6 +14,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SpreadsheetUtils {
 
