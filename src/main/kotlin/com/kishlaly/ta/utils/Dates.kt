ecaae70b8fe231052f5adb5ef151db7964ce7049
kishlalyvo <volodymyr.kishlaly@engelvoelkers.com>
2022-09-07 18:07:18 +0200

    Dates

--- src/main/kotlin/com/kishlaly/ta/utils/Dates.kt
+++ src/main/kotlin/com/kishlaly/ta/utils/Dates.kt
@@ -5,6 +5,8 @@ import com.kishlaly.ta.model.Quote
 import com.kishlaly.ta.model.Timeframe
 import com.kishlaly.ta.model.exchangeTimezome
 import java.time.*
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
 class Dates {
 
@@ -23,27 +25,65 @@ class Dates {
         }
 
         // on the input may be the date of the form yyyyy-MM-dd for the day timeframe, so I shift the clock to the beginning of the exchange
-        fun getTimeInExchangeZone(timestamp: Long, exchangeTimezoneStr: String): ZonedDateTime {
-            val exchangeTimezone = ZoneId.of(exchangeTimezoneStr)
-            val exchangeLocalDateTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(timestamp), exchangeTimezone)
-            val exchangeZonedDateTime = exchangeLocalDateTime.atZone(exchangeTimezone)
-            return exchangeZonedDateTime
+        fun getTimeInExchangeZone(date: String, exchangeTimezone: String): ZonedDateTime {
+            val formatter = DateTimeFormatter.ofPattern(getDateFormat())
+            return when (Context.timeframe) {
+                Timeframe.HOUR -> {
+                    val localDate = LocalDateTime.parse(date, formatter)
+                    localDate
+                            .atZone(ZoneId.of(exchangeTimezone))
+                            .withZoneSameInstant(ZoneId.of(exchangeTimezone))
+                }
+                Timeframe.WEEK, Timeframe.DAY -> LocalDate.parse(date, formatter)
+                        .atStartOfDay(ZoneId.of(exchangeTimezone))
+                        .withZoneSameInstant(ZoneId.of(exchangeTimezone))
+                        .plus(9, ChronoUnit.HOURS)
+                        .plus(30, ChronoUnit.MINUTES)
+            }
         }
 
         // on the input may be the date of the form yyyyy-MM-dd for the day timeframe, so I shift the clock to the beginning of the exchange
-        fun getBarTimeInMyZone(timestamp: Long, exchangeTimezoneStr: String): ZonedDateTime {
-            val exchangeTimezone = ZoneId.of(exchangeTimezoneStr)
-            val myTimezone = ZoneId.of(Context.myTimezone)
-
-            val exchangeLocalDateTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(timestamp), exchangeTimezone)
-            val exchangeZonedDateTime = exchangeLocalDateTime.atZone(exchangeTimezone)
-
-            return exchangeZonedDateTime.withZoneSameInstant(OffsetDateTime.now(myTimezone).offset).withZoneSameInstant(myTimezone)
+        fun getBarTimeInMyZone(date: String, exchangeTimezone: String): ZonedDateTime {
+            val formatter = DateTimeFormatter.ofPattern(getDateFormat())
+            return when (Context.timeframe) {
+                Timeframe.HOUR -> {
+                    val localDate = LocalDateTime.parse(date, formatter)
+                    localDate
+                            .atZone(ZoneId.of(exchangeTimezone))
+                            .withZoneSameInstant(ZoneId.of(Context.myTimezone))
+                }
+                Timeframe.WEEK, Timeframe.DAY -> LocalDate.parse(date, formatter)
+                        .atStartOfDay(ZoneId.of(exchangeTimezone))
+                        .withZoneSameInstant(ZoneId.of(Context.myTimezone))
+                        .plus(9, ChronoUnit.HOURS)
+                        .plus(30, ChronoUnit.MINUTES)
+            }
         }
 
         // At the entrance of the exact time, so there is no need to reset the clock at the time of the opening of the exchange
         fun getBarTimeInMyZone(timestamp: Long, exchangeTimezoneStr: String): ZonedDateTime {
+            val exchangeTimezone = ZoneId.of(exchangeTimezoneStr)
+            val myTimezone = ZoneId.of(Context.myTimezone)
+            val exchangeLocalDateTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(timestamp), exchangeTimezone)
+            val exchangeZonedDateTime = exchangeLocalDateTime.atZone(exchangeTimezone)
+            return exchangeZonedDateTime.withZoneSameInstant(OffsetDateTime.now(myTimezone).offset).withZoneSameInstant(myTimezone)
+        }
 
+        fun getTimeInExchangeZone(timestamp: Long, exchangeTimezoneStr: String): ZonedDateTime {
+            val exchangeTimezone = ZoneId.of(exchangeTimezoneStr)
+            val exchangeLocalDateTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(timestamp!!), exchangeTimezone)
+            return exchangeLocalDateTime.atZone(exchangeTimezone)
+        }
+
+        fun getDuration(timeframe: Timeframe, start: Long, end: Long): String {
+            val timezone = ZoneId.of(Context.myTimezone)
+            val startDate = LocalDateTime.ofInstant(Instant.ofEpochSecond(start), timezone)
+            val endDate = LocalDateTime.ofInstant(Instant.ofEpochSecond(end), timezone)
+            return when (timeframe) {
+                Timeframe.WEEK -> ChronoUnit.WEEKS.between(startDate, endDate).toString() + " weeks"
+                Timeframe.DAY -> ChronoUnit.DAYS.between(startDate, endDate).toString() + " days"
+                Timeframe.HOUR -> ChronoUnit.HOURS.between(startDate, endDate).toString() + " hours"
+            }
         }
 
         fun shortDateToZoned(datePart: String): ZonedDateTime {
