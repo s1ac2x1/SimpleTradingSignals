23901fb5969795d7753016afe0cb5e1f3069b60b
kishlalyvo <volodymyr.kishlaly@engelvoelkers.com>
2022-09-08 08:57:21 +0200

    IndicatorUtils.buildMACDHistogram()

--- src/main/kotlin/com/kishlaly/ta/utils/IndicatorUtils.kt
+++ src/main/kotlin/com/kishlaly/ta/utils/IndicatorUtils.kt
@@ -5,7 +5,10 @@ import com.kishlaly.ta.config.Context
 import com.kishlaly.ta.model.AbstractModel
 import com.kishlaly.ta.model.Quote
 import com.kishlaly.ta.model.indicators.EMA
+import com.kishlaly.ta.model.indicators.MACD
+import org.ta4j.core.BaseBarSeries
 import org.ta4j.core.indicators.EMAIndicator
+import org.ta4j.core.indicators.MACDIndicator
 import org.ta4j.core.indicators.helpers.ClosePriceIndicator
 
 class IndicatorUtils {
@@ -14,8 +17,8 @@ class IndicatorUtils {
 
         fun buildEMA(symbol: String, quotes: List<Quote>, period: Int): List<EMA> {
             val cached = IndicatorsInMemoryCache.getEMA(symbol, Context.timeframe, period)
-            if (!cached.isEmpty()) {
-                return cached
+            return if (!cached.isEmpty()) {
+                cached
             } else {
                 val barSeries = Bars.build(quotes)
                 val closePriceIndicator = ClosePriceIndicator(barSeries)
@@ -28,7 +31,37 @@ class IndicatorUtils {
                 collector = collector.filter { it.valuesPresent() }.toMutableList()
                 var result = trimToDate<EMA>(collector).sortedBy { it.timestamp }
                 IndicatorsInMemoryCache.putEMA(symbol, Context.timeframe, period, result)
-                return result
+                result
+            }
+        }
+
+        fun buildMACDHistogram(symbol: String, quotes: List<Quote>): List<MACD> {
+            val cached = IndicatorsInMemoryCache.getMACD(symbol, Context.timeframe)
+            return if (!cached.isEmpty()) {
+                cached
+            } else {
+                var collector = mutableListOf<MACD>()
+                val barSeries = Bars.build(quotes)
+
+                val closePrice = ClosePriceIndicator(barSeries)
+                val macd = MACDIndicator(closePrice)
+
+                val macdSeries = BaseBarSeries()
+                for (i in 0 until barSeries.barCount) {
+                    macdSeries.addBar(barSeries.getBar(i).endTime, 0.0, 0.0, 0.0,
+                            macd.getValue(i).doubleValue(), 0.0)
+                }
+                val macdSignalIndicator = ClosePriceIndicator(macdSeries)
+                val macdSignal = EMAIndicator(macdSignalIndicator, 9)
+
+                for (i in 0 until barSeries.barCount) {
+                    val histogram = macd.getValue(i).minus(macdSignal.getValue(i)).doubleValue()
+                    collector.add(MACD(quotes[i].timestamp, 0.0, 0.0, histogram))
+                }
+                collector = collector.filter { it.valuesPresent() }.toMutableList()
+                val result = trimToDate(collector).sortedBy { it.timestamp }
+                IndicatorsInMemoryCache.putMACD(symbol, Context.timeframe, result)
+                result
             }
         }
 
