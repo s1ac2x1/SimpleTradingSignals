27b0f095cd8d819f6f48bd131fc00e1c68a53c2b
kishlalyvo <volodymyr.kishlaly@engelvoelkers.com>
2022-09-09 12:55:57 +0200

    Alphavantage

--- src/main/kotlin/com/kishlaly/ta/loaders/Alphavantage.kt
+++ src/main/kotlin/com/kishlaly/ta/loaders/Alphavantage.kt
@@ -1,8 +1,13 @@
 package com.kishlaly.ta.loaders
 
 import com.google.gson.Gson
+import com.google.gson.internal.LinkedTreeMap
+import com.google.gson.reflect.TypeToken
 import com.kishlaly.ta.model.Quote
 import com.kishlaly.ta.model.Timeframe
+import com.kishlaly.ta.utils.Quotes
+import com.squareup.okhttp.OkHttpClient
+import com.squareup.okhttp.Request
 
 /**
  * For stocks, the last price is always the end of the previous day, regardless of the timeframe
@@ -30,9 +35,48 @@ class Alphavantage {
         fun loadQuotes(symbol: String, timeframe: Timeframe): List<Quote> {
             val quotes = mutableListOf<Quote>()
             val url = getQuotesUrl(symbol, timeframe)
+            try {
+                getResponse(url)?.let { respone ->
+                    var key = when (timeframe) {
+                        Timeframe.WEEK -> "Weekly Time Series"
+                        Timeframe.DAY -> "Time Series (Daily)"
+                        Timeframe.HOUR -> "Time Series (60min)"
+                    }
+                    respone.get(key)?.forEach { k, v ->
+                        val day = k
+                        val open = v.get["1. open"]
+                        val high = v.get("2. high")
+                        val low = v.get("3. low")
+                        val close = v.get("4. close")
+                        val volume = v.get("5. volume")
+
+                    }
+                } ?: return emptyList()
+
+            } catch (e: Exception) {
+                println(e.message)
+                return emptyList<Quote>()
+            }
+            if (quotes.size < Quotes.resolveMinBarsCount(timeframe)) {
+                return emptyList<Quote>()
+            }
+            return quotes.sortedBy { it.timestamp }
         }
 
-        private fun getQuotesUrl(symbol: String, timeframe: Timeframe): String {
+        fun getResponse(url: String): LinkedTreeMap<String, LinkedTreeMap<String, String>>? {
+            return try {
+                val httpClient = OkHttpClient()
+                val request = Request.Builder().url(url).get().build()
+                val body = httpClient.newCall(request).execute().body()
+                gson.fromJson<LinkedTreeMap<String, LinkedTreeMap<String, String>>>(body.string(),
+                        object : TypeToken<LinkedTreeMap<String, LinkedTreeMap<String, String>>>() {}.type)
+            } catch (e: Exception) {
+                null
+            }
+        }
+
+
+        fun getQuotesUrl(symbol: String, timeframe: Timeframe): String {
             val prefix = "https://www.alphavantage.co/query?function="
             val url: String
             url = when (timeframe) {
