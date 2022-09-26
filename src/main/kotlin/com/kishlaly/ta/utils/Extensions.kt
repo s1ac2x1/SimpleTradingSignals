32234d16fd62963357a61d2b38011768f0acfd25
kishlalyvo <volodymyr.kishlaly@engelvoelkers.com>
2022-09-26 13:52:47 +0200

    update

--- src/main/kotlin/com/kishlaly/ta/utils/Extensions.kt
+++ src/main/kotlin/com/kishlaly/ta/utils/Extensions.kt
@@ -4,13 +4,13 @@ import com.kishlaly.ta.model.SymbolData
 import java.math.BigDecimal
 
 fun Double.round(): Double {
-    return if (this == Double.NaN || this == 0.0) {
+    return if (this.isNaN() || this.isInfinite()) {
         0.0
     } else BigDecimal.valueOf(this).setScale(2, BigDecimal.ROUND_UP).toDouble()
 }
 
 fun Double.roundDown(): Double {
-    return if (this == Double.NaN || this == 0.0) {
+    return if (this.isNaN() || this.isInfinite()) {
         0.0
     } else BigDecimal.valueOf(this).setScale(2, BigDecimal.ROUND_DOWN).toDouble()
 }
