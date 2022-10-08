c6060fb3e223769b46b9d6ae3ede9fa81829ef23
Vladimir Kishlaly <vladimir@kishlaly.com>
2022-10-08 14:01:55 +0200

    update

--- src/main/kotlin/com/kishlaly/ta/MonteCarlo.kt
+++ src/main/kotlin/com/kishlaly/ta/MonteCarlo.kt
@@ -11,6 +11,7 @@ import com.kishlaly.ta.analyze.testing.sl.StopLossFixedPrice
 import com.kishlaly.ta.analyze.testing.tp.TakeProfitFixedKeltnerTop
 import com.kishlaly.ta.config.Context
 import com.kishlaly.ta.utils.DBUtils
+import java.util.stream.StreamSupport
 
 fun main() {
     DBUtils.initDB()
@@ -23,29 +24,28 @@ fun main() {
         "com.kishlaly.ta.analyze.tasks.blocks.two",
         clazz = ScreenTwoBlock::class.java
     )
-    var i = 0
-    outer@ for (screenOneCombination in screenOneGenerator) {
-        for (screenTwoCombination in screenTwoGenerator) {
-            if (screenOneCombination.vector.size > 0 && screenTwoCombination.vector.size > 0) {
-                println("Testing blocks with [${screenOneCombination.size}][${screenTwoCombination.size}]...")
-                TaskTester.testOneStrategy(
-                    Context.basicTimeframes,
-                    TaskType.THREE_DISPLAYS_BUY,
-                    GeneratedBlocksGroup(
-                        listOf(ScreenBasicValidation()),
-                        screenOneCombination.vector,
-                        screenTwoCombination.vector
-                    ),
-                    StopLossFixedPrice(0.27),
-                    TakeProfitFixedKeltnerTop(80)
-                )
-                i++
-                if (i > 5) {
-                    break@outer
+
+    val limit = 5L
+    StreamSupport.stream(screenOneGenerator.spliterator(), false)
+        .filter { it.size > 0 }
+        .limit(limit)
+        .forEach { screenOneCombination ->
+            StreamSupport.stream(screenTwoGenerator.spliterator(), false)
+                .filter { it.size > 0 }
+                .limit(limit)
+                .forEach { screenTwoCombination ->
+                    TaskTester.testOneStrategy(
+                        Context.basicTimeframes,
+                        TaskType.THREE_DISPLAYS_BUY,
+                        GeneratedBlocksGroup(
+                            listOf(ScreenBasicValidation()),
+                            screenOneCombination.vector,
+                            screenTwoCombination.vector
+                        ),
+                        StopLossFixedPrice(0.27),
+                        TakeProfitFixedKeltnerTop(80)
+                    )
                 }
-            } else {
-                println("Skipped [${screenOneCombination.size}][${screenTwoCombination.size}]")
-            }
+
         }
-    }
 }
