d467c7482933ded5b3c6b1683662117928344772
kishlalyvo <volodymyr.kishlaly@engelvoelkers.com>
2022-10-10 09:35:23 +0200

    update

---
+++ src/main/kotlin/com/kishlaly/ta/analyze/testing/MonteCarloBasic.kt
@@ -0,0 +1,22 @@
+package com.kishlaly.ta.analyze.testing
+
+import com.kishlaly.ta.cache.CacheReader
+import com.kishlaly.ta.config.Context
+import com.kishlaly.ta.model.SymbolsSource
+import com.kishlaly.ta.model.Timeframe
+import com.kishlaly.ta.utils.DBUtils
+import com.kishlaly.ta.utils.RunUtils
+
+abstract class MonteCarloBasic {
+
+    constructor(symbol: String) {
+        Context.aggregationTimeframe = Timeframe.DAY
+        Context.source = arrayOf(SymbolsSource.SP500)
+        RunUtils.singleSymbol(symbol)
+        Context.symbols = CacheReader.getSymbols()
+        DBUtils.initDB()
+    }
+
+    abstract fun run()
+
+}
---
+++ src/main/kotlin/com/kishlaly/ta/analyze/testing/MonteCarloStrategies.kt
@@ -0,0 +1,50 @@
+package com.kishlaly.ta.analyze.testing
+
+import com.kishlaly.ta.analyze.TaskType
+import com.kishlaly.ta.analyze.tasks.blocks.commons.ScreenBasicValidation
+import com.kishlaly.ta.analyze.tasks.blocks.one.ScreenOneBlock
+import com.kishlaly.ta.analyze.tasks.blocks.two.ScreenTwoBlock
+import com.kishlaly.ta.analyze.tasks.groups.BlockGroupsUtils
+import com.kishlaly.ta.analyze.tasks.groups.GeneratedBlocksGroup
+import com.kishlaly.ta.analyze.testing.sl.StopLossFixedPrice
+import com.kishlaly.ta.analyze.testing.tp.TakeProfitFixedKeltnerTop
+import com.kishlaly.ta.config.Context
+import java.util.stream.StreamSupport
+
+class MonteCarloStrategies(symbol: String, val limit: Long) : MonteCarloBasic(symbol) {
+
+    override fun run() {
+        val screenOneGenerator = BlockGroupsUtils().generateBlocksCombinations(
+            "com.kishlaly.ta.analyze.tasks.blocks.one",
+            clazz = ScreenOneBlock::class.java
+        )
+        val screenTwoGenerator = BlockGroupsUtils().generateBlocksCombinations(
+            "com.kishlaly.ta.analyze.tasks.blocks.two",
+            clazz = ScreenTwoBlock::class.java
+        )
+
+        StreamSupport.stream(screenOneGenerator.spliterator(), false)
+            .filter { it.size > 0 }
+            .limit(limit)
+            .forEach { screenOneCombination ->
+                StreamSupport.stream(screenTwoGenerator.spliterator(), false)
+                    .filter { it.size > 0 }
+                    .limit(limit)
+                    .forEach { screenTwoCombination ->
+                        println("Testing [${screenOneCombination.vector.size}][${screenTwoCombination.vector.size}]...")
+                        TaskTester.testOneStrategy(
+                            Context.basicTimeframes,
+                            TaskType.THREE_DISPLAYS_BUY,
+                            GeneratedBlocksGroup(
+                                listOf(ScreenBasicValidation()),
+                                screenOneCombination.vector,
+                                screenTwoCombination.vector
+                            ),
+                            StopLossFixedPrice(0.27),
+                            TakeProfitFixedKeltnerTop(80)
+                        )
+                    }
+            }
+    }
+
+}
