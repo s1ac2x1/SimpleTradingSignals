9122bf9020d68865940d573898a57c296af61255
Vladimir Kishlaly <vladimir@kishlaly.com>
2022-10-19 14:46:10 +0200

    update

--- src/main/kotlin/com/kishlaly/ta/Main.kt
+++ src/main/kotlin/com/kishlaly/ta/Main.kt
@@ -1,6 +1,7 @@
 package com.kishlaly.ta
 
 import com.kishlaly.ta.analyze.TaskType
+import com.kishlaly.ta.analyze.tasks.groups.threedisplays.ThreeDisplays_Buy_4
 import com.kishlaly.ta.analyze.tasks.groups.threedisplays.ThreeDisplays_Buy_EFI_2
 import com.kishlaly.ta.analyze.testing.TaskTester
 import com.kishlaly.ta.analyze.testing.sl.StopLossFixedPrice
@@ -23,7 +24,7 @@ fun main() {
     TaskTester.testOneStrategy(
         Context.basicTimeframes.get(),
         TaskType.THREE_DISPLAYS_BUY,
-        ThreeDisplays_Buy_EFI_2(),
+        ThreeDisplays_Buy_4(),
         StopLossFixedPrice(0.27),
         TakeProfitFixedKeltnerTop(70)
     )
