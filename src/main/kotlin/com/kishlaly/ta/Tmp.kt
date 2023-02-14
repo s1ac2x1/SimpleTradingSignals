e6e9789995c4ef37f919137ddcd65c5b021e3263
Vladimir Kishlaly <vladimir@kishlaly.com>
2023-02-14 21:00:06 +0100

    update

--- src/main/kotlin/com/kishlaly/ta/Tmp.kt
+++ src/main/kotlin/com/kishlaly/ta/Tmp.kt
@@ -6,8 +6,17 @@ import com.kishlaly.ta.model.indicators.ElderForceIndex
 import com.kishlaly.ta.model.indicators.Indicator
 
 fun main() {
-    val screen2 = CacheReader.getSymbolData(TaskType.THREE_DISPLAYS_BUY.getTimeframeIndicators(2), "LUMN")
-    println(screen2.lastQuote.nativeDate)
-    val efi: List<ElderForceIndex> = screen2.indicators.get(Indicator.EFI) as List<ElderForceIndex>
-    println(efi)
+//    val screen2 = CacheReader.getSymbolData(TaskType.THREE_DISPLAYS_BUY.getTimeframeIndicators(2), "LUMN")
+//    println(screen2.lastQuote.nativeDate)
+//    val efi: List<ElderForceIndex> = screen2.indicators.get(Indicator.EFI) as List<ElderForceIndex>
+//    println(efi)
+    println(fibonacci(10))
+}
+
+fun fibonacci(n: Int): Int {
+    return when (n) {
+        0 -> 0
+        1 -> 1
+        else -> fibonacci(n - 1) + fibonacci(n - 2)
+    }
 }
---
+++ src/main/kotlin/com/kishlaly/ta/openai/Main.kt
@@ -0,0 +1,4 @@
+package com.kishlaly.ta.openai
+
+class Main {
+}
