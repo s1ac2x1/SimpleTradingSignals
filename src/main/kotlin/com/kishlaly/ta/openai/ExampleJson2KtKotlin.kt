ce0f65f3ac0257cf66924d0045f5fb4c1758b15a
Vladimir Kishlaly <vladimir@kishlaly.com>
2023-02-16 20:33:37 +0100

    update

--- src/main/kotlin/com/kishlaly/ta/openai/ExampleJson2KtKotlin.kt
+++ src/main/kotlin/com/kishlaly/ta/openai/ExampleJson2KtKotlin.kt
@@ -2,10 +2,10 @@ package com.kishlaly.ta.openai
 
 import com.google.gson.annotations.SerializedName
 
-data class ExampleJson2KtKotlin(
+data class CompletionRespone(
 
     @SerializedName("id") var id: String? = null,
-    @SerializedName("object") var object: String? = null,
+    @SerializedName("object") var obj: String? = null,
     @SerializedName("created") var created: Int? = null,
     @SerializedName("model") var model: String? = null,
     @SerializedName("choices") var choices: ArrayList<Choices> = arrayListOf(),
--- src/main/kotlin/com/kishlaly/ta/openai/Main.kt
+++ src/main/kotlin/com/kishlaly/ta/openai/Main.kt
@@ -1,6 +1,8 @@
 package com.kishlaly.ta.openai
 
+import com.google.common.reflect.TypeToken
 import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
 import com.squareup.okhttp.MediaType
 import com.squareup.okhttp.OkHttpClient
 import com.squareup.okhttp.Request
@@ -15,17 +17,20 @@ fun main() {
     val request = CompletionRequest(
         prompt = "Please write a detailed article about what are the negative effects of yerba mate. Use this information to write the article: In the U.S., yerba mate is widely available in health food stores and online. People who recommend yerba mate say that it can relieve fatigue, aid in weight loss, ease depression, and help treat headaches and various other conditions. There's limited evidence that yerba mate may help with some of these conditions."
     )
-    getCompletion(request)
+    val completion = getCompletion(request)
+    println(completion)
 }
 
 data class CompletionRequest(
     val model: String = "text-davinci-003",
     val prompt: String,
-    val max_tokens: Int = 2048,
-    val top_p: Double = 0.5
+    @SerializedName("max_tokens")
+    val maxTokens: Int = 2048,
+    @SerializedName("top_p")
+    val topP: Double = 0.5
 )
 
-fun getCompletion(completionRequest: CompletionRequest): String {
+fun getCompletion(completionRequest: CompletionRequest): String? {
     return try {
         val httpClient = OkHttpClient()
         httpClient.setConnectTimeout(5, TimeUnit.MINUTES)
@@ -38,7 +43,8 @@ fun getCompletion(completionRequest: CompletionRequest): String {
             .header("Authorization", "Bearer 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")
             .build()
         val body = httpClient.newCall(request).execute().body().string()
-        return body
+        val completionRespone = gson.fromJson<CompletionRespone>(body, object : TypeToken<CompletionRespone>() {}.type)
+        return completionRespone.choices.firstOrNull()?.text
     } catch (e: Exception) {
         ""
     }
