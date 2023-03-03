82206cd203dc68ec38cb70140f926c7fe9939299
Vladimir Kishlaly <vladimir@kishlaly.com>
2023-03-03 20:58:02 +0100

    update

--- src/main/kotlin/com/kishlaly/ta/openai/flow/Blogpost.kt
+++ src/main/kotlin/com/kishlaly/ta/openai/flow/Blogpost.kt
@@ -83,12 +83,14 @@ fun main() {
 //    )
 
     // пишет текс об этом противоположном мнении
-    val step_7_prompt = File("$outputFolder/step_6_1").readText()
-    Step(
-        "7",
-        listOf("$step_7_prompt Schreiben Sie zwei Absätze zu diesem Thema"),
-        listOf(createParagraphs, trimmed)
-    )
+//    val step_7_prompt = File("$outputFolder/step_6_1").readText()
+//    Step(
+//        "7",
+//        listOf("$step_7_prompt Schreiben Sie zwei Absätze zu diesem Thema"),
+//        listOf(createParagraphs, trimmed)
+//    )
+
+    // собираем вопросы для FAQ - опциональный шаг, можно не генерить
 
     // картинка (вероятно, лучше по step_2 elements)
 
