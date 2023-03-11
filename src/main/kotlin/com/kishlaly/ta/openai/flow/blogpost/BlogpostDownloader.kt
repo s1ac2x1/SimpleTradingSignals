ea326764f457b7aa82c1455f39a7faa7c15d1116
Vladimir Kishlaly <vladimir@kishlaly.com>
2023-03-11 18:21:25 +0100

    update

--- src/main/kotlin/com/kishlaly/ta/openai/flow/blogpost/BlogpostDownloader.kt
+++ src/main/kotlin/com/kishlaly/ta/openai/flow/blogpost/BlogpostDownloader.kt
@@ -6,20 +6,6 @@ import com.kishlaly.ta.openai.mainOutputFolder
 import java.io.File
 import kotlin.random.Random
 
-fun main() {
-    val text = """
-Die Geschichte des Kratzens ist eine der ältesten menschlichen Beziehungen zu Katzen. Seit Jahrhunderten haben Menschen versucht, die natürliche Anziehungskraft zwischen Katzen und Menschen zu erforschen. Kratzen ist ein natürliches Verhalten bei Katzen, das sie als Mittel verwenden, um ihre Umgebung zu markieren und ihr Territorium abzustecken. 
-
-Es wird angenommen, dass es für sie auch eine Art von Entspannung und Stressabbau sein kann. Einige Forscher glauben sogar, dass Kratzen Teil der natürlichen sozialen Interaktion zwischen Mensch und Tier sein könnte. In den frühesten Aufzeichnungen über Katzen wurde berichtet, dass Kratzer als Geschenke an die Götter geschickt wurden - als Zeichen des Respekts vor dem mystischen Wesen der Katze. 
-
-Dieser Brauch existiert noch heute in vielerlei Form in vielerlei Ländern auf der ganzen Welt. Kratzer haben auch eine lange Geschichte im Umgang mit Haustieren: Sie wurden verwendet, um den Tieren beizubringen, wo sie schlafen oder essen sollten; um ihnen beizubringen, wo sie ihr Geschäft machen sollten; oder man benutzte sie als Mittel der Bestrafung für unerwünschtes Verhalten. Heutzutage versteht man jedoch besser die Bedeutung von Kratzern für die psychische und emotionale Gesundheit von Haustieren - sowohl beim Menschen als auch beim Tier - und es gibt spezielle Produkte auf dem Markt (Kratzbäume usw. 
-
-), die diesem Zweck dienlich sein könnnen. Obwohl es schwer ist zu bestimmen, warum Katzen kratzen oder warum Menschen angezogen werden von diesem Verhalten – was immer der Grund sein mag – bleibt Kratzen eines der älteststen Beispiele für den tiefgreifendsten Teil unserer Beziehung mit unseren geliebten Fellfreundinnnen: Das Verständnis und Respekt voreinander!.
-""".trimIndent()
-    val result = createParagraphs(text)
-    println(result)
-}
-
 class BlogpostDownloader(val meta: BlogpostContentMeta) {
 
     private val stepFolder = "$mainOutputFolder/${meta.keyword.toFileName()}"
