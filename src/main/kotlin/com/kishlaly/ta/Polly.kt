60e45956fae8cb3c9eec488dfecee58990a8839f
Vladimir Kishlaly <vladimir@kishlaly.com>
2023-03-23 21:12:55 +0100

    update

--- src/main/kotlin/com/kishlaly/ta/Polly.kt
+++ src/main/kotlin/com/kishlaly/ta/Polly.kt
@@ -8,24 +8,30 @@ fun main() {
         .take(1)
         .forEach { chunk ->
             chunk.forEach { line ->
-                val command = mutableListOf<String>()
-                command.add("aws")
-                command.add("polly")
-                command.add("synthesize-speech")
-                command.add("--output-format")
-                command.add("mp3")
-                command.add("--voice-id")
-                command.add("Tatyana")
-                command.add("--text-type")
-                command.add("ssml")
-                command.add("--text")
-                val split = line.split("###")
-                command.add("<speak><lang xml:lang=\"ru-RU\">${split[1]}</lang><break time=\"1000ms\"/></speak>")
-                command.add("random_ru_${count++}.mp3")
-                runAwsPollyCommand(command)
+                generate(line, count, "Tatyana", "ru-RU", 1, "ru")
                 Thread.sleep(300)
             }
     }
+}
+
+private fun generate(line: String, count: Int, voice: String, lang: String, lineIndex: Int, prefix: String) {
+    var count1 = count
+    val command = mutableListOf<String>()
+    command.add("aws")
+    command.add("polly")
+    command.add("synthesize-speech")
+    command.add("--output-format")
+    command.add("mp3")
+    command.add("--voice-id")
+    command.add(voice)
+    command.add("Hans")
+    command.add("--text-type")
+    command.add("ssml")
+    command.add("--text")
+    val split = line.split("###")
+    command.add("<speak><lang xml:lang=\"$lang\">${split[lineIndex]}</lang><break time=\"1000ms\"/></speak>")
+    command.add("polly/random_${prefix}_${count1++}.mp3")
+    runAwsPollyCommand(command)
 }
 
 fun runAwsPollyCommand(command: List<String>) {
