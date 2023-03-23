43259cb5852f2784913f88085561e255e1d3bee1
Vladimir Kishlaly <vladimir@kishlaly.com>
2023-03-23 21:16:21 +0100

    update

--- src/main/kotlin/com/kishlaly/ta/Polly.kt
+++ src/main/kotlin/com/kishlaly/ta/Polly.kt
@@ -3,21 +3,22 @@ package com.kishlaly.ta
 import java.io.File
 
 fun main() {
+    val mainName = "random"
     var count = 1
     File("polly.txt").readLines().chunked(20)
         .take(1)
         .forEach { chunk ->
             chunk.forEach { line ->
-                generate(line, count, "Tatyana", "ru-RU", 1, "ru")
+                generate(line, count, "Tatyana", "ru-RU", 1, "ru", mainName)
                 Thread.sleep(100)
                 count = 1
-                generate(line, count, "Hans", "de-DE", 0, "de")
+                generate(line, count, "Hans", "de-DE", 0, "de", mainName)
                 Thread.sleep(100)
             }
     }
 }
 
-private fun generate(line: String, count: Int, voice: String, lang: String, lineIndex: Int, prefix: String) {
+private fun generate(line: String, count: Int, voice: String, lang: String, lineIndex: Int, prefix: String, mainName: String) {
     var count1 = count
     val command = mutableListOf<String>()
     command.add("aws")
@@ -27,13 +28,12 @@ private fun generate(line: String, count: Int, voice: String, lang: String, line
     command.add("mp3")
     command.add("--voice-id")
     command.add(voice)
-    command.add("Hans")
     command.add("--text-type")
     command.add("ssml")
     command.add("--text")
     val split = line.split("###")
     command.add("<speak><lang xml:lang=\"$lang\">${split[lineIndex]}</lang><break time=\"1000ms\"/></speak>")
-    command.add("polly/random_${prefix}_${count1++}.mp3")
+    command.add("polly/${mainName}_${prefix}_${count1++}.mp3")
     runAwsPollyCommand(command)
 }
 
