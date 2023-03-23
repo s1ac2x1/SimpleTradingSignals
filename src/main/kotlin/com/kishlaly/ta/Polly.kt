0622e79d77c9ebad3d26a3bffe64cb07dd3a3021
Vladimir Kishlaly <vladimir@kishlaly.com>
2023-03-23 20:47:34 +0100

    update

--- src/main/kotlin/com/kishlaly/ta/Polly.kt
+++ src/main/kotlin/com/kishlaly/ta/Polly.kt
@@ -1,15 +1,35 @@
 package com.kishlaly.ta
 
 import java.io.File
-import java.lang.StringBuilder
 
 fun main() {
-    val request = StringBuilder("aws polly synthesize-speech --output-format mp3 --voice-id Hans --text-type ssml --text '<speak>")
-    File("polly.txt").readLines().take(1).forEach { line ->
-        val split = line.split("###")
-        request.append("<lang xml:lang=\"ru-RU\">${split[1]}</lang><break time=\"1000ms\"/>")
-        request.append("<lang xml:lang=\"de-DE\">${split[0]}</lang><break time=\"2500ms\"/>")
+    var count = 1
+    File("polly.txt").readLines().chunked(20)
+        .take(1)
+        .forEach { chunk ->
+            val request = StringBuilder("aws polly synthesize-speech --output-format mp3 --voice-id Hans --text-type ssml --text '<speak>")
+            chunk.forEach { line ->
+                val split = line.split("###")
+                request.append("<lang xml:lang=\"ru-RU\">${split[1]}</lang><break time=\"1000ms\"/>")
+                request.append("<lang xml:lang=\"de-DE\">${split[0]}</lang><break time=\"2500ms\"/>")
+            }
+            request.append("</speak>' random_${count++}.mp3")
+            runAwsPollyCommand(request.toString())
     }
-    request.append("</speak>' output.mp3")
-    File("polly_request.txt").writeBytes(request.toString().toByteArray())
+}
+
+fun runAwsPollyCommand(command: String) {
+    val processBuilder = ProcessBuilder(*command.split(" ").toTypedArray())
+    processBuilder.redirectErrorStream(true)
+    processBuilder.directory(File(System.getProperty("user.dir")))
+
+    val process = processBuilder.start()
+    val output = process.inputStream.bufferedReader().readText()
+    val exitCode = process.waitFor()
+
+    if (exitCode == 0) {
+        println("Command executed successfully:\n$output")
+    } else {
+        println("Error executing command:\n$output")
+    }
 }
